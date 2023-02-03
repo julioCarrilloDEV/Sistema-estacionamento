ba2fe1add0f69b9e7977aab1e2ad64197a670a75
julioCarrilloDEV <jc.carrillodev@gmail.com>
2023-02-03 16:59:52 -0300

    Implementando o projeto

---
+++ script.js
@@ -0,0 +1,50 @@
+"use strict";
+(function () {
+    var _a;
+    const $ = (query) => document.querySelector(query);
+    function patio() {
+        function ler() {
+            return localStorage.patio ? JSON.parse(localStorage.patio) : []; //Ler as informações no banco do próprio navegador;
+        }
+        function adicionar(veiculo, salva) {
+            var _a;
+            const row = document.createElement("tr");
+            row.innerHTML = `
+            <td>${veiculo.nome}</td>
+            <td>${veiculo.placa}</td>
+            <td>${veiculo.entrada}</td>
+            <td>
+            <button class="delete" data-placa="${veiculo.placa}">X</button>
+            </td>
+            `;
+            if (salva)
+                (_a = $("#patio")) === null || _a === void 0 ? void 0 : _a.appendChild(row);
+            if (salva)
+                salvar([...ler(), veiculo]); // Recarrega as informações dos carros anteriores. 
+        }
+        function remover() {
+        }
+        function salvar(veiculos) {
+            localStorage.setItem("patio", JSON.stringify(veiculos)); //Salvar as informações
+        }
+        function render() {
+            $("#patio").innerHTML = "";
+            const patio = ler();
+            if (patio.length) {
+                patio.forEach(veiculo => adicionar(veiculo));
+            }
+        }
+        return { ler, adicionar, remover, salvar, render };
+    }
+    patio().render();
+    (_a = $("#cadastrar")) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => {
+        var _a, _b;
+        const nome = (_a = $('nome')) === null || _a === void 0 ? void 0 : _a.value;
+        const placa = (_b = $('placa')) === null || _b === void 0 ? void 0 : _b.value;
+        if (!nome || !placa) {
+            alert("Atenção: Os campos nome e placa são obrigatórios!");
+            return;
+        }
+        patio().adicionar({ nome, placa, entrada: new Date() }, true);
+    });
+})();
